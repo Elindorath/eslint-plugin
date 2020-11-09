8fec8c83662cc80d114a0f03a8f7b5326e9da0ad
Joachim Westphal <elindorath@gmail.com>
2020-11-09 15:32:43 +0100

    initial

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+const OFF = 'off';
+const WARN = 'warn';
+const ERROR = 'error';
+
+
+module.exports = {
+  plugins: [
+    '@elindorath',
+  ],
+  extends: [
+    'plugin:@elindorath/project-eslint-plugin',
+  ],
+};
