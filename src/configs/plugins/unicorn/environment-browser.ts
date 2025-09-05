0230bdc9264036ce8bf6eb32f70534f93cbe8ea8
Joachim Westphal <elindorath@gmail.com>
2025-09-05 22:57:47 +0200

    added new rules from eslint-plugin-unicorn

--- src/configs/plugins/unicorn/environment-browser.ts
+++ src/configs/plugins/unicorn/environment-browser.ts
@@ -14,6 +14,7 @@ export const unicornBrowserConfig = {
     'unicorn/no-document-cookie': [ERROR],
     'unicorn/no-invalid-remove-event-listener': [ERROR],
     'unicorn/prefer-blob-reading-methods': [ERROR],
+    'unicorn/prefer-classlist-toggle': [ERROR],
     'unicorn/prefer-dom-node-append': [ERROR],
     'unicorn/prefer-dom-node-dataset': [ERROR],
     'unicorn/prefer-dom-node-remove': [ERROR],
